c45c69df9df74835f9702bfdddb876a8a952abef
wanly13 <wanlyobregon13@gmail.com>
2024-07-02 21:52:11 -0500

    mucha

---
+++ Assets/BUTTONS/button_next.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class button_next : MonoBehaviour
+{
+    // Método público que será llamado cuando se haga clic en el botón
+    public void OnButtonClick()
+    {
+        Debug.Log("BOTON PRESIONADO.");
+        // Aquí puedes agregar la lógica que quieres que ocurra cuando se presiona el botón
+        // Por ejemplo, cargar la siguiente escena, cambiar un valor en el juego, etc.
+    }
+}
--- Assets/Videos/scriptIntroVideo.cs
+++ Assets/Videos/scriptIntroVideo.cs
@@ -5,18 +5,29 @@ using UnityEngine.SceneManagement;
 public class VideoEndHandler : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    public string nextSceneName;
+    //public string nextSceneName;
+    public GameObject tutorialCanvas;
+
 
     void Start()
     {
+        //tutorialCanvas.SetActive(false);
         videoPlayer.loopPointReached += OnVideoEnd;
     }
 
     void OnVideoEnd(VideoPlayer vp)
     {
         gameObject.SetActive(false);
-        //&Debug.Log("string");
-        //SceneManager.LoadScene(nextSceneName);
+            if (tutorialCanvas != null)
+            {
+                Debug.LogWarning("Abre of canvas.");
+                tutorialCanvas.SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning("No se ha asignado el canvas del tutorial en el inspector.");
+            }
+
     }
    
 }
